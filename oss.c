745a0c92feb340406828b4cd4b1b062e3f64914b
Jesse N. Richardson <richardson183@bellsouth.net>
2010-11-02 17:29:33 -0400

    Fixed up the indentation of the oss.c file

--- oss.c
+++ oss.c
@@ -1,28 +1,28 @@
 /*
 
-	Audio Overload SDK
+  Audio Overload SDK
 
-	Copyright (c) 2007, R. Belmont and Richard Bannister.
+  Copyright (c) 2007, R. Belmont and Richard Bannister.
 
-	All rights reserved.
+  All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-	* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-	* Neither the names of R. Belmont and Richard Bannister nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+  * Neither the names of R. Belmont and Richard Bannister nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdio.h>
@@ -70,196 +70,196 @@ static INT16 samples[44100*2];
 
 void m1sdr_SetSamplesPerTick(UINT32 spf)
 {
-	nDSoundSegLen = spf;
+  nDSoundSegLen = spf;
 }
 
 // m1sdr_Update - timer callback routine: runs sequencer and mixes sound
 
 void m1sdr_Update(void)
-{	
-	if (!hw_present) return;
+{
+  if (!hw_present) return;
 
-	if (m1sdr_Callback)
-	{
-		m1sdr_Callback(nDSoundSegLen, (INT16 *)samples);
-	}
+  if (m1sdr_Callback)
+    {
+      m1sdr_Callback(nDSoundSegLen, (INT16 *)samples);
+    }
 }
 // checks the play position to see if we should trigger another update
 
 void m1sdr_TimeCheck(void)
 {
 #if VALGRIND
-	m1sdr_Update();
+  m1sdr_Update();
 #else
-	audio_buf_info info;
+  audio_buf_info info;
 
-    	ioctl(audiofd, SNDCTL_DSP_GETOSPACE, &info);
+  ioctl(audiofd, SNDCTL_DSP_GETOSPACE, &info);
 
-	if (oss_nw)
+  if (oss_nw)
+    {
+      int err;
+
+      m1sdr_Update();
+      playtime++;
+
+      // output the generated samples
+      err = write(audiofd, samples, nDSoundSegLen * 4);
+      if (err == -1)
 	{
-		int err;
-
-		m1sdr_Update();
-		playtime++;
-
-		// output the generated samples
-		err = write(audiofd, samples, nDSoundSegLen * 4);
-		if (err == -1)
-		{
-			perror("write\n");
-		}
-
-		#if LOG_WAVE
-		fwrite(samples, nDSoundSegLen*4, 1, logfil);
-		#endif
-	}
-	else
-	{
-	    	while (info.bytes >= (nDSoundSegLen * 4))
-		{
-			m1sdr_Update();
-			playtime++;
-
-			// output the generated samples
-			write(audiofd, samples, nDSoundSegLen * 4);
-
-			#if LOG_WAVE
-			fwrite(samples, nDSoundSegLen*4, 1, logfil);
-			#endif
-
-		    	ioctl(audiofd, SNDCTL_DSP_GETOSPACE, &info);
-		}
+	  perror("write\n");
 	}
 
-	usleep(50);
+#if LOG_WAVE
+      fwrite(samples, nDSoundSegLen*4, 1, logfil);
+#endif
+    }
+  else
+    {
+      while (info.bytes >= (nDSoundSegLen * 4))
+	{
+	  m1sdr_Update();
+	  playtime++;
+
+	  // output the generated samples
+	  write(audiofd, samples, nDSoundSegLen * 4);
+
+#if LOG_WAVE
+	  fwrite(samples, nDSoundSegLen*4, 1, logfil);
+#endif
+
+	  ioctl(audiofd, SNDCTL_DSP_GETOSPACE, &info);
+	}
+    }
+
+  usleep(50);
 #endif
 }
 
 // m1sdr_Init - inits the output device and our global state
 
 INT16 m1sdr_Init(int sample_rate)
-{	
-	int format, stereo, rate, fsize;
+{
+  int format, stereo, rate, fsize;
 
-	hw_present = 0;
+  hw_present = 0;
 
-	nDSoundSegLen = sample_rate / 60;
+  nDSoundSegLen = sample_rate / 60;
 
-	memset(samples, 0, 44100*4);	// zero out samples
+  memset(samples, 0, 44100*4);	// zero out samples
 
-	m1sdr_Callback = NULL;
+  m1sdr_Callback = NULL;
 
-	audiofd = open("/dev/dsp", O_WRONLY, 0);
-	if (audiofd == -1)
+  audiofd = open("/dev/dsp", O_WRONLY, 0);
+  if (audiofd == -1)
+    {
+      perror("/dev/dsp");
+
+      audiofd = open("/dev/dsp1", O_WRONLY, 0);
+
+      if (audiofd == -1)
 	{
-		perror("/dev/dsp");
-
-		audiofd = open("/dev/dsp1", O_WRONLY, 0);
-
-		if (audiofd == -1)
-		{
-			perror("/dev/dsp1");
-			return(0);
-		}
+	  perror("/dev/dsp1");
+	  return(0);
 	}
+    }
 
-	// reset things
-	ioctl(audiofd, SNDCTL_DSP_RESET, 0);
+  // reset things
+  ioctl(audiofd, SNDCTL_DSP_RESET, 0);
 
-	is_broken_driver = 0;
-	num_frags = NUM_FRAGS_NORMAL;
+  is_broken_driver = 0;
+  num_frags = NUM_FRAGS_NORMAL;
 
-	// set the buffer size we want
-	fsize = OSS_FRAGMENT;
-	if (ioctl(audiofd, SNDCTL_DSP_SETFRAGMENT, &fsize) == - 1)
-	{
-	perror("SNDCTL_DSP_SETFRAGMENT");
-	return(0);
-	}
+  // set the buffer size we want
+  fsize = OSS_FRAGMENT;
+  if (ioctl(audiofd, SNDCTL_DSP_SETFRAGMENT, &fsize) == - 1)
+    {
+      perror("SNDCTL_DSP_SETFRAGMENT");
+      return(0);
+    }
 
-	// set 16-bit output
-	format = AFMT_S16_NE;	// 16 bit signed "native"-endian
-	if (ioctl(audiofd, SNDCTL_DSP_SETFMT, &format) == - 1)
-	{
-			perror("SNDCTL_DSP_SETFMT");
-		return(0);
-	}
+  // set 16-bit output
+  format = AFMT_S16_NE;	// 16 bit signed "native"-endian
+  if (ioctl(audiofd, SNDCTL_DSP_SETFMT, &format) == - 1)
+    {
+      perror("SNDCTL_DSP_SETFMT");
+      return(0);
+    }
 
-	// now set stereo
-	stereo = 1;
-	if (ioctl(audiofd, SNDCTL_DSP_STEREO, &stereo) == - 1)
-	{
-			perror("SNDCTL_DSP_STEREO");
-		return(0);
-	}
+  // now set stereo
+  stereo = 1;
+  if (ioctl(audiofd, SNDCTL_DSP_STEREO, &stereo) == - 1)
+    {
+      perror("SNDCTL_DSP_STEREO");
+      return(0);
+    }
 
-	// and the sample rate
-	rate = sample_rate;
-	if (ioctl(audiofd, SNDCTL_DSP_SPEED, &rate) == - 1)
-	{
-			perror("SNDCTL_DSP_SPEED");
-		return(0);
-	}
+  // and the sample rate
+  rate = sample_rate;
+  if (ioctl(audiofd, SNDCTL_DSP_SPEED, &rate) == - 1)
+    {
+      perror("SNDCTL_DSP_SPEED");
+      return(0);
+    }
 
-	// and make sure that did what we wanted
-	ioctl(audiofd, SNDCTL_DSP_GETBLKSIZE, &fsize);
-	//printf("Fragment size: %d\n", fsize);
+  // and make sure that did what we wanted
+  ioctl(audiofd, SNDCTL_DSP_GETBLKSIZE, &fsize);
+  //printf("Fragment size: %d\n", fsize);
 
-	hw_present = 1;
+  hw_present = 1;
 
 #if LOG_WAVE
-	logfil = fopen("log.bin", "wb");
+  logfil = fopen("log.bin", "wb");
 #endif
 
-	return (1);
+  return (1);
 }
 
 void m1sdr_Exit(void)
-{	
-	if (!hw_present) return;
+{
+  if (!hw_present) return;
 
-	close(audiofd);
+  close(audiofd);
 #if LOG_WAVE
-	fclose(logfil);
+  fclose(logfil);
 #endif
 }
 
 void m1sdr_SetCallback(void *fn)
 {
-	if (fn == (void *)NULL)
-	{
-		printf("ERROR: NULL CALLBACK!\n");
-	}
+  if (fn == (void *)NULL)
+    {
+      printf("ERROR: NULL CALLBACK!\n");
+    }
 
-//	printf("m1sdr_SetCallback: aok!\n");
-	m1sdr_Callback = (void (*)(unsigned long, signed short *))fn;
+  //	printf("m1sdr_SetCallback: aok!\n");
+  m1sdr_Callback = (void (*)(unsigned long, signed short *))fn;
 }
 
 INT16 m1sdr_IsThere(void)
 {
-	audiofd = open("/dev/dsp", O_WRONLY, 0);
+  audiofd = open("/dev/dsp", O_WRONLY, 0);
 
-	if (audiofd == -1)
-	{
-		printf("Error accessing soundcard, sound will be disabled\n");
-		hw_present = 0;
-		return(0);
-	}
+  if (audiofd == -1)
+    {
+      printf("Error accessing soundcard, sound will be disabled\n");
+      hw_present = 0;
+      return(0);
+    }
 
-	close(audiofd);
-	hw_present = 1;
-	return (1);
+  close(audiofd);
+  hw_present = 1;
+  return (1);
 }
 
 INT32 m1sdr_HwPresent(void)
 {
-	return hw_present;
+  return hw_present;
 }
 
 // unused stubs for this driver, but the Win32 driver needs them
 void m1sdr_PlayStart(void)
 {
-	playtime = 0;
+  playtime = 0;
 }
 
 void m1sdr_PlayStop(void)
@@ -268,23 +268,22 @@ void m1sdr_PlayStop(void)
 
 void m1sdr_FlushAudio(void)
 {
-	memset(samples, 0, nDSoundSegLen * 4);
-	write(audiofd, samples, nDSoundSegLen * 4);
-	write(audiofd, samples, nDSoundSegLen * 4);
+  memset(samples, 0, nDSoundSegLen * 4);
+  write(audiofd, samples, nDSoundSegLen * 4);
+  write(audiofd, samples, nDSoundSegLen * 4);
 }
 
 void m1sdr_SetNoWait(int nw)
 {
-	oss_nw = nw;
+  oss_nw = nw;
 }
 
 short *m1sdr_GetSamples(void)
 {
-	return samples;
+  return samples;
 }
 
 int m1sdr_GetPlayTime(void)
 {
-	return playtime;
+  return playtime;
 }
-
